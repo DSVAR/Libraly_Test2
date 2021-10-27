b9e3e2e806fa7ec0bb135ad8809257b0fb00332f
Davar <danilka070103@gmail.com>
2021-10-27 18:15:06 +0300

    nav

--- Libraly_Test2/ClientApp/src/Components/Navbar/Navbar.jsx
+++ Libraly_Test2/ClientApp/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Links from "./Components/Links";
 import Classes from "./Navbar.module.css";
+import Input from "../../StyledComponents/LittleComponents/Input";
 
 const Navbar = () => {
     return (
         <nav className={Classes.navbar}>
-            <Links/>
+            <Input />
         </nav>
     );
 };
