7fd2685387c50f7e16855eabaf329f2d252633c0
DVAR <danilka070103@gmail.com>
2021-10-28 06:51:06 +0300

    Animation Input1

--- Libraly_Test2/ClientApp/src/StyledComponents/LittleComponents/Input.jsx
+++ Libraly_Test2/ClientApp/src/StyledComponents/LittleComponents/Input.jsx
@@ -1,41 +1,64 @@
 import React from 'react';
-import styled,{css} from 'styled-components'
+import styled, {css, keyframes} from 'styled-components'
 
-const StyledInput=styled.input`
-  background: ${props=> props.color || 'aqua'};
-  text-align:center;
-  border:green solid 0.5vh ;
-  border-radius:2rem;
-  margin:1vh auto;
+const Animation=keyframes`
+  //from{
+  //
+  //}
+  //to{
+  //  
+  //}
+  0%,100%{
+    border-style: solid;
+    border-color: red darkred aquamarine aqua;
+    box-shadow: ;
+
+  }
+  50%{
+    border-style: solid;
+    border-color: aqua aquamarine darkred red;
+  }
+`
+
+const StyledInput = styled.input`
+  background: ${props => props.background || 'lightgray'};
+  text-align: center;
+  border: solid 0.4vh ${props => props.borderColor || 'green'};
+  border-radius: 2rem;
+  margin: 1vh auto;
   height: 30%;
+  box-shadow: -1em -1em 3em aqua,
+                -1em 1em 3em aquamarine,
+                1em 1em 3em darkred,
+                1em -1em 3em red ;
+  outline: 0;
+  outline-offset: 0;
   
-  outline:0;
-  outline-offset:0;
-  :active{
-    background: red;
-    border: none;
-    outline:none;
+  
+  
+  :active {
+    outline: none;    
+  }
 
+  :focus {
+    //box-shadow: -0.8em 0.3em  1em lightgray;
+    background: lightgray;
+    //border: solid 0.3vh;
+    outline: none;
+    animation: ${Animation} linear infinite 2s;
   }
-  :focus{
-    height: 50%z;
-    background: orange; 
-    border: 0;
-    outline:none;
-    
-  }
-  :hover{
-    
+
+  :hover {
+  
     border-color: red;
-    outline:none;
-    
+    outline: none;
+
   }
 `;
 
 
-
 const Input = (props) => {
-    return ( <StyledInput {...props} children={props.children}   />
+    return (<StyledInput {...props} children={props.children}/>
 
     );
 };
